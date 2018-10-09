1fcb4af40d2e1c6c61a4a2ed1d7e77e41b1cdfba
Serhii Slobodian <beinc.dn@gmail.com>
2018-10-09 18:22:55 +0300

    [BUGFIX] Fix electron updater error on mac

--- server/utils/updater.js
+++ server/utils/updater.js
@@ -1,4 +1,4 @@
-const { dialog } = require("electron");
+const { dialog, app, BrowserWindow } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const isDev = require("electron-is-dev");
 const baseLogger = require("./logger");
@@ -9,22 +9,25 @@ const logger = baseLogger.child("binaries");
 
 logger.debug("[UPDATER]: isDev", isDev);
 
-const updaterManager = (window) => {
-    const mainWindow = window;
+const MANUAL_UPDATE_CHECK = "MANUAL_UPDATE_CHECK";
+const AUTO_UPDATE_CHECK = "AUTO_UPDATE_CHECK";
 
+const updaterManager = () => {
     let menu;
+    let isManual = false;
 
     const init = () => {
         logger.debug("[UPDATER]: Init");
         autoUpdater.checkForUpdates();
-        const oneHour = 60 * 60 * 1000;
-        setInterval(() => autoUpdater.checkForUpdates(), oneHour);
+        const oneDay = 24 * 60 * 60 * 1000;
+        setInterval(() => autoUpdater.checkForUpdates(), oneDay);
     };
 
-    const manualCheckUpdate = (menuItem, focusedWindow, event) => {
+    const manualCheckUpdate = (menuItem) => {
         logger.debug("[UPDATER]: Click on update from menu");
         menu = menuItem;
         menu.enabled = false;
+        isManual = true;
         autoUpdater.checkForUpdates();
     };
 
@@ -33,10 +36,11 @@ const updaterManager = (window) => {
         if (!menu) {
             return;
         }
+        isManual = false;
         if (error.statusCode === 404) {
             dialog.showMessageBox({
-                title: "Update not found",
-                message: "Update not found",
+                title: "Update was not found",
+                message: "There is no update available. Your wallet application is up to date.",
             });
         } else {
             dialog.showErrorBox("Error: ", !error ? "unknown" : (error.stack).toString());
@@ -47,21 +51,25 @@ const updaterManager = (window) => {
 
     autoUpdater.on("update-not-available", () => {
         logger.debug("[UPDATER]: Update not found");
-        dialog.showMessageBox({
-            title: "Update not found",
-            message: "Update not found",
-        });
+        if (isManual) {
+            dialog.showMessageBox({
+                title: "Update was not found",
+                message: "There is no update available. Your wallet application is up to date.",
+            });
+            isManual = false;
+        }
         menu.enabled = true;
         menu = null;
     });
 
     autoUpdater.on("update-available", () => {
         logger.debug("[UPDATER]: Update found");
+        isManual = false;
         dialog.showMessageBox({
             type: "info",
-            title: "Update found",
-            message: "Found update, do you want to update now?",
-            buttons: ["Now", "Later"],
+            title: "Update the LightningPeach wallet",
+            message: "New update is available. Update now?",
+            buttons: ["Update now", "Remind me later"],
         }, (buttonIndex) => {
             if (buttonIndex === 0) {
                 logger.debug("[UPDATER]: User accept to update");
@@ -77,9 +85,16 @@ const updaterManager = (window) => {
 
     autoUpdater.on("update-downloaded", () => {
         logger.debug("[UPDATER]: Update downloaded");
+        app.removeAllListeners("window-all-closed");
+        const browserWindows = BrowserWindow.getAllWindows();
+        browserWindows.forEach((browserWindow) => {
+            browserWindow.removeAllListeners("close");
+        });
+        autoUpdater.autoInstallOnAppQuit = true;
         dialog.showMessageBox({
-            title: "Update downloaded",
-            message: "Update downloaded, application will quit for update.",
+            title: "Update is downloaded",
+            message: process.platform === "darwin" ? "Update is downloaded. Quit the application to update."
+                : "Update is downloaded. The wallet application will be closed for updating.",
         }, () => {
             logger.debug("[UPDATER]: Update will be installed");
             setImmediate(() => autoUpdater.quitAndInstall());
