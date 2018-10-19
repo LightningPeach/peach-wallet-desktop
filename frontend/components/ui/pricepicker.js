6a9f5155018af59769c0196c93c40a36dcf565fc
Serhii Slobodian <beinc.dn@gmail.com>
2018-10-19 13:05:45 +0300

    [FEATURE] Filter
    Add response connection functionality by props for picker components

--- frontend/components/ui/pricepicker.js
+++ frontend/components/ui/pricepicker.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import isEqual from "lodash/isEqual";
 import DigitsField from "components/ui/digitsField";
 
 class Pricepicker extends Component {
@@ -21,19 +22,38 @@ class Pricepicker extends Component {
         document.addEventListener("touchend", this.handleTouchEnd);
     }
 
+    componentWillReceiveProps(nextProps) {
+        const { from, to, currency } = nextProps.price;
+        if (!isEqual(nextProps.price, this.props.price)) {
+            this.setState({
+                currency,
+                from,
+                to,
+            });
+        }
+    }
+
     componentWillUnmount() {
         document.removeEventListener("keyup", this.handleKeyUp);
         document.removeEventListener("mouseup", this.handleMouseUp);
         document.removeEventListener("touchend", this.handleTouchEnd);
     }
 
-    setFromPrice = (e) => {
+    setData = () => {
+        this.props.setData({
+            currency: this.state.currency,
+            from: this.state.from,
+            to: this.state.to,
+        });
+    };
+
+    setFromPrice = () => {
         this.setState({
             from: this.priceFrom.value.trim(),
         });
     };
 
-    setToPrice = (e) => {
+    setToPrice = () => {
         this.setState({
             to: this.priceTo.value.trim(),
         });
@@ -75,19 +95,18 @@ class Pricepicker extends Component {
         });
     };
 
-    reset = () => {
-        this.priceFromComponent.reset();
-        this.priceToComponent.reset();
+    handleCancel = () => {
+        const { from, to, currency } = this.props.price;
+        const { bitcoinMeasureType } = this.props;
         this.setState({
-            currency: this.props.bitcoinMeasureType,
-            from: null,
-            showInput: false,
-            to: null,
+            currency: currency || bitcoinMeasureType,
+            from: from || null,
+            to: to || null,
         });
     };
 
     render() {
-        const { className, bitcoinMeasureType } = this.props;
+        const { className, bitcoinMeasureType, reset } = this.props;
         return (
             <div className="picker">
                 <button
@@ -110,6 +129,7 @@ class Pricepicker extends Component {
                         <DigitsField
                             id="price__from"
                             className="form-text form-text--long"
+                            value={this.state.from}
                             pattern={this.state.currency === "Satoshi"
                                 ? "above_zero_int"
                                 : "above_zero_float"}
@@ -131,6 +151,7 @@ class Pricepicker extends Component {
                         <DigitsField
                             id="price__to"
                             className="form-text form-text--long"
+                            value={this.state.to}
                             pattern={this.state.currency === "Satoshi"
                                 ? "above_zero_int"
                                 : "above_zero_float"}
@@ -172,20 +193,20 @@ class Pricepicker extends Component {
                     <div className="picker__row picker__row--controls">
                         <button
                             className="button button__link"
-                            onClick={this.reset}
+                            onClick={reset}
                         >
                             Reset
                         </button>
                         <div className="picker__group">
                             <button
                                 className="button button__link"
-                                onClick={this.hideInput}
+                                onClick={this.handleCancel}
                             >
                                 Cancel
                             </button>
                             <button
                                 className="button button__link"
-                                onClick={this.hideInput}
+                                onClick={this.setData}
                             >
                                 Ok
                             </button>
@@ -201,6 +222,13 @@ class Pricepicker extends Component {
 Pricepicker.propTypes = {
     bitcoinMeasureType: PropTypes.string.isRequired,
     className: PropTypes.string,
+    price: PropTypes.shape({
+        currency: PropTypes.string.isRequired,
+        from: PropTypes.number.isRequired,
+        to: PropTypes.number.isRequired,
+    }).isRequired,
+    reset: PropTypes.func.isRequired,
+    setData: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
