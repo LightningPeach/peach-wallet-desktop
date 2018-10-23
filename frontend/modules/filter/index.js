b150b893a6dbdbb6424c65ff523b4bb9ee267d97
Serhii Slobodian <beinc.dn@gmail.com>
2018-10-23 19:29:04 +0300

    [FEATURE] Filter
    Move filtering function to filter module

--- frontend/modules/filter/index.js
+++ frontend/modules/filter/index.js
@@ -1,10 +1,12 @@
 import filterReducer from "./reducers";
 
+import * as filterOperations from "./operations";
 import * as filterActions from "./actions";
 import * as filterTypes from "./types";
 
 export {
     filterActions,
+    filterOperations,
     filterTypes,
 };
 
