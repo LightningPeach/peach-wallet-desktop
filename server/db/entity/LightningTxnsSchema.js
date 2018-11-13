89f577d644b06f304ce2c99704c5b0b63fde41c9
Eugene B <gamergektor@gmail.com>
2018-11-13 19:20:45 +0200

    remove name from lightningtxns schema

--- server/db/entity/LightningTxnsSchema.js
+++ server/db/entity/LightningTxnsSchema.js
@@ -3,7 +3,6 @@ const { EntitySchema } = require("typeorm");
 const { LightningTxns } = require("../model/LightningTxns");
 
 module.exports = new EntitySchema({
-    name: "LightningTxns",
     target: LightningTxns,
     columns: {
         paymentHash: {
