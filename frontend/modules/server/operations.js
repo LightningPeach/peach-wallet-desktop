47c267f31092a210e9eb6506e69143968f7cbf3c
Serhii Slobodian <beinc.dn@gmail.com>
2019-02-01 14:38:27 +0200

    [BUGFIX] Update behavior in merchnts logo url

--- frontend/modules/server/operations.js
+++ frontend/modules/server/operations.js
@@ -31,7 +31,6 @@ function getMerchants() {
         try {
             response = await fetch(url);
             response = await response.json();
-            response = response.map(m => ({ ...m, logo: PEACH_API_HOST + m.logo }));
             dispatch(actions.merchantsSuccess(response));
         } catch (e) {
             logger.error(e.message);
